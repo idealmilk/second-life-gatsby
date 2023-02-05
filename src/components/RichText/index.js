643aec58b17f8919c635553e3e49f970de0d9039
Matthew Gilligan <mcgilligan97@gmail.com>
2023-02-05 16:03:25 +0900

    Update rich text component

--- src/components/RichText/index.js
+++ src/components/RichText/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
@@ -15,17 +16,12 @@ const RichText = (body) => {
         </div>
       ),
       [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
-      [BLOCKS.EMBEDDED_ASSET]: (node) => (
-        <div>
-          <img
-            src={`https:${node.data.target.fields.file['en-US'].url}`}
-            alt=''
-          />
-          {node.data.target.fields.description && (
-            <p>{node.data.target.fields.description['en-US']}</p>
-          )}
-        </div>
-      ),
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const { gatsbyImageData, description } = node.data.target;
+        return (
+          <GatsbyImage image={getImage(gatsbyImageData)} alt={description} />
+        );
+      },
       [INLINES.HYPERLINK]: (node) => {
         if (node.data.uri.indexOf('youtube.com/embed') !== -1) {
           return (
@@ -64,8 +60,11 @@ const RichText = (body) => {
         }
       },
     },
-    renderText: (text) =>
-      text.split('\n').flatMap((text, i) => [i > 0 && <br />, text]),
+    renderText: (text) => {
+      return text.split('\n').reduce((children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      }, []);
+    },
   };
 
   return <div>{renderRichText(body, options)}</div>;
